0a720a29971b56deee618694818d5fc50c73d10e
Vignesh Kanamatareddy <94678521+Vignesh234@users.noreply.github.com>
2022-04-01 09:51:31 +0000

    yyyyy

--- p2final.c
+++ p2final.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+void input_line(float *x1,float *y1,float *x2,float *y2,float *x3,float *y3)
+{
+  printf("Enter the coordinates\n");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+}
+int is_triangle(float x1,floaty1,float x2,float y2,float x3,float y3)
+{
+  float area = 0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+    if(area>0)
+      return 1;
+      return 0;
+}
+void output(int is_triangle)
+{
+  if(is_triangle)
+  printf("Three lines form a triangle\n");
+  else
+    printf("Three lines do not form a triangle\n");
+}
--- p3final.c
+++ p3final.c
@@ -1,1 +1,33 @@
-
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter the number\n");
+  scanf("%d",&n);
+  return n;
+}
+int is_prime(int n)
+{
+  int f=2;
+  while(f<=n/2)
+    {
+      if(n%f==0)
+        return 0;
+      f++;
+    }
+    return 1;
+}
+void output(int n,int prime)
+{
+  if(is_prime(n))
+    printf("The number%d is a prime\n",n);
+  else
+    printf("The number is not prime\n")
+}
+int main()
+{
+  int n=input_number();
+  int prime=is_prime(n);
+  output(n,prime);
+  return 0;
+}
