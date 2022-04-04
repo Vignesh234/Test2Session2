7040dff6502aedf9afb2038dcb50faa7602a00d8
Vignesh Kanamatareddy <94678521+Vignesh234@users.noreply.github.com>
2022-04-04 05:51:29 +0000

    yyyy

--- p5final.c
+++ p5final.c
@@ -1,1 +1,42 @@
-
+#include<stdio.h>
+#include<math.h>
+int input_array_size()
+{
+  int n;
+  printf("Enter the numbers\n");
+  scanf("%d",&n);
+  return n;
+}
+void init_array(int n,int a[n])
+{
+  for(int i=0;i<n;i++)
+    a[i]=i;
+    a[1]=0;
+}
+void erotosthenes_sieve(int n, int a[n])
+{
+  int i=0;
+  while(i<sqrt(n))
+    {
+      for(i;a[i]==0;i++);
+      for(int k=i+i;k<n;k+=i)
+        a[i]=0;
+      i++;
+    }
+}
+void output(int n,int a[n])
+{
+  for(int i=0;i<n;i++)
+    if(a[i]!=0)
+      printf("%d",a[i]);
+}
+int main()
+{
+  int n;
+  n=input_array_size();
+  int a[n];
+  init_array(n,a);
+  erotosthenes_sieve(n,a);
+  output(n,a);
+  return 0;
+}
